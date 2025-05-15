202f5240a81929b98004e4d354d748116b8db33b
underJGdeployment <joschagabrielacm@gmail.com>
2025-05-15 21:02:42 +0700

    update push - copy clipboard share & mobile ver hotline & tollfree

--- link-script.js
+++ link-script.js
@@ -1,39 +1,57 @@
 const linkElement = document.querySelector('#hotline'); 
-const linkElementEmail = document.querySelector('#email');
+const linkElementTollfree = document.querySelector('#tollfree');
+const linkElementShare = document.querySelector('#share'); 
 
-linkElement.addEventListener('click', (event) => {
-  if (window.innerWidth > 800) {
-    // If screen width is greater than 800px
-    event.preventDefault(); // Prevent default link behavior
-    navigator.clipboard.writeText(linkElement.href)
-      .then(() => {
-        alert('Hotline Number copied to clipboard!'); 
-      })
-      .catch(err => {
-        console.error('Failed to copy: ', err);
-        alert('Failed to copy link to clipboard.'); 
+      linkElement.addEventListener('click', (event) => {
+        if (window.innerWidth > 800) {
+          // If screen width is greater than 800px
+          event.preventDefault(); // Prevent default link behavior
+          navigator.clipboard.writeText(linkElement.href)
+            .then(() => {
+              alert('Hotline Number copied to clipboard!'); 
+            })
+            .catch(err => {
+              console.error('Failed to copy: ', err);
+              alert('Failed to copy link to clipboard.'); 
+            });
+        } else {
+          // If screen width is less than or equal to 800px
+          // Allow default link behavior (open in new tab)
+        }
       });
-  } else {
-    // If screen width is less than or equal to 800px
-    // Allow default link behavior (open in new tab)
-  }
-});
 
-linkElementEmail.addEventListener('click', (event) => {
-    if (window.innerWidth > 800) {
-      // If screen width is greater than 800px
-      event.preventDefault(); // Prevent default link behavior
-      navigator.clipboard.writeText(linkElementEmail.href)
-        .then(() => {
-          alert('Email Address copied to clipboard!'); 
-        })
-        .catch(err => {
-          console.error('Failed to copy: ', err);
-          alert('Failed to copy link to clipboard.'); 
+      linkElementShare.addEventListener('click', (event) => {
+        if (window.innerWidth > 100) {
+          // If screen width is greater than 800px
+          event.preventDefault(); // Prevent default link behavior
+          navigator.clipboard.writeText(linkElementShare.href)
+            .then(() => {
+              alert('Multipaga landing page link copied to clipboard!'); 
+            })
+            .catch(err => {
+              console.error('Failed to copy: ', err);
+              alert('Failed to copy link to clipboard.'); 
+            });
+        } else {
+          // If screen width is less than or equal to 800px
+          // Allow default link behavior (open in new tab)
+        }
+      });
+
+      linkElementTollfree.addEventListener('click', (event) => {
+          if (window.innerWidth > 800) {
+            // If screen width is greater than 800px
+            event.preventDefault(); // Prevent default link behavior
+            navigator.clipboard.writeText(linkElementTollfree.href)
+              .then(() => {
+                alert('Tollfree number copied to clipboard!'); 
+              })
+              .catch(err => {
+                console.error('Failed to copy: ', err);
+                alert('Failed to copy link to clipboard.'); 
+              });
+          } else {
+            // If screen width is less than or equal to 800px
+            // Allow default link behavior (open in new tab)
+          }
         });
-    } else {
-      // If screen width is less than or equal to 800px
-      // Allow default link behavior (open in new tab)
-    }
-  });
-  
